b1c95b426ec5617b498eb62158aaddf585a7f01a
misspia <pia_leung@hotmail.com>
2018-02-23 16:56:36 -0800

    webpack setup

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const paths = {
+	DIST: path.resolve(__dirname, 'dist'),
+	SRC: path.resolve(__dirname, 'src'),
+};
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: path.join(paths.SRC, 'index.js'),
+	output: {
+		path: paths.DIST,
+		filename: 'bundle.js',
+		publicPath: 'dist/'
+	},
+	plugins: [
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpg|jpeg|gif|ico)$/,
+				use: [
+					'file-loader',
+				],
+			},
+      {
+        test: /\.(glsl|vert|frag)$/,
+        loader: 'webpack-glsl-loader'
+      },
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					'babel-loader',
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+}
