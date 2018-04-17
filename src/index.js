68f80c8ccf7abddcd30cf49ce92443b1311bcb1d
misspia <pia_leung@hotmail.com>
2018-04-16 19:28:17 -0700

    pass in shaders

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import './index.scss'
 import App from './js/app.js'
 import Stage from './js/stage.js'
-import frag from './shaders/frag.glsl'
 
 const canvas = document.getElementById('canvas');
 const options = {
--- src/js/modules/bar.js
+++ src/js/modules/bar.js
@@ -8,24 +8,42 @@ class Bar {
     this.height = 1.0;
     this.width = 0.5;
     this.depth = 0.5;
+    this.vert = vert;
+    this.frag = frag;
+    this.geometry = {};
+    this.material = {};
 
     this.mesh = {};
     this.init();
   }
   init() {
-    const geometry = new THREE.BoxGeometry(this.width, this.height, this.depth);
-    const material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
-
-    this.mesh = new THREE.Mesh(geometry, material);
+    this.initGeometry();
+    this.initMaterial();
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.setPos();
+  }
+  initGeometry() {
+    this.geometry = new THREE.BoxBufferGeometry(this.width, this.height, this.depth);
+    this.geometry.addAttribute('frequency', new THREE.Float32BufferAttribute(1, 1, 1, 1));
+  }
+  initMaterial() {
+    this.material = new THREE.RawShaderMaterial({
+        uniforms: {
+            time: { value: 1.0 },
+            resolution: { value: new THREE.Vector2() },
+        },
+        vertexShader: this.vert,
+        fragmentShader: this.frag,
+    });
   }
   setPos() {
     this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
   }
   update(node) {
     this.mesh.scale.y = Math.max(0.1, node / 255) * 5;
+    this.updateShaderAttributes();
   }
-  render() {
+  updateShaderAttributes() {
 
   }
 }
